04cf87d1e981864da9a96fef1cd83c5a57cd492f
zzzxt0 <250350232@qq.com>
2020-07-09 01:45:20 +0800

    test

---
+++ Game_of_life/src/Game_of_life/Main.java
@@ -0,0 +1,16 @@
+package Game_of_life;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			mainJFrame JFrame=new mainJFrame();
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
